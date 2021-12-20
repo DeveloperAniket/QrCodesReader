e6c0af458a417595f041abe8d347431a350a1c9d
Aniket Samanta <Aniket.Samanta@coxautoinc.com>
2021-12-20 16:01:31 +0530

    action button issue and redeem

--- projects/zxing-scanner-demo/src/app/QR-Details/qr-detail/qr-detail.component.ts
+++ projects/zxing-scanner-demo/src/app/QR-Details/qr-detail/qr-detail.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 import { QrCodeModel } from '../../shared/model/qrcode.model';
 import { QrApiService } from '../../shared/services/qr-api.service';
+import { Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-qr-detail',
@@ -13,6 +14,7 @@ export class QrDetailComponent implements OnInit, OnChanges {
   qrCodeDetail: QrCodeModel;
   informationText: String = "QR code found";
   loading: boolean = false;
+  @Output() backToScan = new EventEmitter<boolean>();
   constructor(private _qrApiService: QrApiService) { }
 
   ngOnInit(): void {
@@ -22,7 +24,6 @@ export class QrDetailComponent implements OnInit, OnChanges {
     if (changes && changes.data.currentValue !== changes.data.previousValue && this.data.Id !== 0
       && this.data.Code !== null && this.data.Code !== '') {
       this.getCodeDetail();
-      this.populateInfoMessage();
     }
   }
   private populateInfoMessage() {
@@ -41,7 +42,10 @@ export class QrDetailComponent implements OnInit, OnChanges {
     this.loading = true;
     this._qrApiService.getQRcode(this.data.Code).subscribe((response) => {                           //next() callback
       console.log(response)
-      this.qrCodeDetail = response;
+      if (response) {
+        this.qrCodeDetail = response;
+        this.populateInfoMessage();
+      }
     },
       (error) => {                              //error() callback
         console.error('Request failed with error')
@@ -50,12 +54,47 @@ export class QrDetailComponent implements OnInit, OnChanges {
       },
       () => {                                   //complete() callback
         this.loading = false;
-      })
+      });
   }
   public onRedeem() {
-    console.log("Reedem");
+    this.loading = true;
+    this._qrApiService.markQRcodeRedeem(this.data.Code).subscribe((response) => {                           //next() callback
+      console.log(response)
+      if (response) {
+        this.qrCodeDetail = response;
+        this.populateInfoMessage();
+      }
+    },
+      (error) => {                              //error() callback
+        console.error('Request failed with error')
+        this.informationText = error;
+        this.loading = false;
+      },
+      () => {                                   //complete() callback
+        this.loading = false;
+      });
   }
   public onActive() {
-    console.log("Active");
+    this.loading = true;
+    this._qrApiService.markQRcodeIssue(this.data.Code).subscribe((response) => {                           //next() callback
+      console.log(response)
+      if (response) {
+        console.log(response)
+        this.qrCodeDetail = response;
+        this.populateInfoMessage();
+      }
+    },
+      (error) => {                              //error() callback
+        console.error('Request failed with error')
+        this.informationText = error;
+        this.loading = false;
+      },
+      () => {                                   //complete() callback
+        this.loading = false;
+      });
+  }
+
+  public onBackClick() {
+    this.backToScan.emit(true);
   }
 }
--- projects/zxing-scanner-demo/src/app/shared/services/qr-api.service.ts
+++ projects/zxing-scanner-demo/src/app/shared/services/qr-api.service.ts
@@ -13,8 +13,32 @@ export class QrApiService {
   constructor(private http: HttpClient) { }
 
   getQRcode(code: string): Observable<QrCodeModel> {
-    return this.http.get<QrCodeModel>(this.baseURL + code)
-      .pipe(map(result => Object.assign(new QrCodeModel(), result)));
+    return this.http.get(this.baseURL + code)
+      .pipe(map(result => this.mapQrCodeModel(result)));
 
+  }
+  markQRcodeIssue(code: string): Observable<QrCodeModel> {
+    return this.http.get(this.baseURL + 'issue/' + code)
+      .pipe(map(result => this.mapQrCodeModel(result)));
+
+  }
+  markQRcodeRedeem(code: string): Observable<QrCodeModel> {
+    return this.http.get(this.baseURL + 'redeem/' + code)
+      .pipe(map(result => this.mapQrCodeModel(result)));
+
+  }
+
+  private mapQrCodeModel(string: any): QrCodeModel {
+    const res = new QrCodeModel();
+    if (string) {
+      res.Id = string["id"];
+      res.Code = string["code"];
+      res.IsDeleted = string["isDeleted"];
+      res.IsIssued = string["isIssued"];
+      res.IsRedeemed = string["isRedeemed"];
+      res.RedeedemDate = string["redeedemDate"];
+      res.Created = string["created"];
+    }
+    return res;
   }
 }
